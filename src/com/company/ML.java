5bd169b009e39a8939ae54138548736b132dfcaf
Shinkai91 <yosiaaser5@gmail.com>
2022-03-27 11:08:27 +0800

    Tokopeds

---
+++ src/com/company/ML.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class ML {
+    private String Nomor, Nama;
+    private String[][] setdata = new String[5][2];
+
+    public ML(String id, String nick) {
+        Nomor = id;
+        Nama = nick;
+        String[][] setdata = {
+                {"2112012", "Pekcuy"},
+                {"234512", "xiao"},
+                {"120123", "Yotsuba"},
+                {"3454231", "Akusiapa"},
+                {"1233432", "GojoSatoru"}
+        };
+        this.setdata = setdata;
+    }
+
+    private boolean Pesanan() {
+        for (int i = 0; i < setdata.length; i++) {
+            if (setdata[i][0].equals(Nomor)) {
+                if (setdata[i][1].equals(Nama)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void transaksi() {
+        Boolean Trans = Pesanan();
+        if (Trans == true) {
+            System.out.println("Transaksi Berhasil");
+            System.out.println("ID : " + Nomor);
+            System.out.println("NICK : " + Nama);
+        } else {
+            System.out.println("Masukan ID dan Nick dengan benar");
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,60 @@
+package com.company;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.time.*;
+
+public class Main {
+    static void harga(int data) {
+        if (data == 1) {
+            System.out.println("\n133\uD83D\uDC8E - 31K");
+        } else if (data == 2) {
+            System.out.println("\n266\uD83D\uDC8E - 63K");
+        } else if (data == 3) {
+            System.out.println("\n400\uD83D\uDC8E - 94K");
+        } else if (data == 4) {
+            System.out.println("\n532\uD83D\uDC8E - 125K");
+        } else if (data == 5) {
+            System.out.println("\n670\uD83D\uDC8E - 156K");
+        } else if (data == 6) {
+            System.out.println("\n1342\uD83D\uDC8E - 311K");
+        } else if (data == 7) {
+            System.out.println("\n2700\uD83D\uDC8E - 622K");
+        } else {
+            System.out.println("nominal salah");
+        }
+    }
+
+    static void list() {
+        System.out.println("list Diamond Mobile Legends");
+        System.out.println("1. 133\uD83D\uDC8E - 31K\n" +
+                "2. 266\uD83D\uDC8E - 63K\n" +
+                "3. 400\uD83D\uDC8E - 94K\n" +
+                "4. 534\uD83D\uDC8E - 125K\n" +
+                "5. 670\uD83D\uDC8E - 156K\n" +
+                "6. 1342\uD83D\uDC8E - 311K\n" +
+                "7. 2700\uD83D\uDC8E - 622K");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        list();
+        Thread.sleep(5000);
+        System.out.println("\nSelamat Datang di Tokoped Shop");
+        System.out.println("Top up Games Mobile Legends");
+        System.out.print("Masukan ID : ");
+        String ID = sc.next();
+        System.out.print("Masukan NICK : ");
+        String NICK = sc.next();
+        System.out.print("Masukan Nominal : ");
+        int NOM = sc.nextInt();
+
+        ML Trans = new ML(ID, NICK);
+
+        harga(NOM);
+        Trans.transaksi();
+        LocalDateTime waktu = LocalDateTime.now();
+        System.out.println(waktu.format(DateTimeFormatter.ofPattern("dd-MM-yyyy \nHH:mm:ss")));
+        Thread.sleep(1000);
+        System.out.println("\nDatang lagi yah mazzeh :)");
+    }
+}
